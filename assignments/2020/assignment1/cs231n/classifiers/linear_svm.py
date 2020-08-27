37f6df70ea446705db524036a81b365e758fab78
Dennis Imhof <dennis.imhof@stud.h-da.de>
2020-08-27 22:16:52 +0200

    added svm loop and svm vec, still needs svm grad vectorized

--- assignments/2020/assignment1/cs231n/classifiers/linear_svm.py
+++ assignments/2020/assignment1/cs231n/classifiers/linear_svm.py
@@ -28,6 +28,7 @@ def svm_loss_naive(W, X, y, reg):
     num_train = X.shape[0]
     loss = 0.0
     for i in range(num_train):
+        label_true = y[i]
         scores = X[i].dot(W)
         correct_class_score = scores[y[i]]
         for j in range(num_classes):
@@ -36,6 +37,8 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1 # note delta = 1
             if margin > 0:
                 loss += margin
+                dW[:,j] += X[i]
+                dW[:,label_true] -= X[i]
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
@@ -53,9 +56,10 @@ def svm_loss_naive(W, X, y, reg):
     # code above to compute the gradient.                                       #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
-
+    
+    dW /= num_train
+    dW += reg * 2 * W
+    
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     
     return loss, dW
@@ -67,6 +71,17 @@ def svm_loss_vectorized(W, X, y, reg):
     Structured SVM loss function, vectorized implementation.
 
     Inputs and outputs are the same as svm_loss_naive.
+    
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
+
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
     """
     loss = 0.0
     dW = np.zeros(W.shape) # initialize the gradient as zero
@@ -78,8 +93,32 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
-
+    num_train = X.shape[0]
+    
+    # Scores for each sample and svm-classifier
+    scores = X.dot(W) # N x C
+    
+    # Select the svm-clf of the true label for each sample and subtract its score
+    # from each column
+    scores -= scores[range(len(y)), y, None]
+    
+    # Add the hardcoded delta = 1
+    scores += 1
+    
+    # Set the scores = 0 for the svm-clfs of the true labels -> they should not
+    # contribute to the loss
+    scores[range(len(y)),y] = 0
+    
+    # Apply the max function -> this one is faster than np.maximum
+    scores *= scores > 0
+    #scores = np.maximum(0, scores)
+    
+    # Average the loss of the batch
+    loss = scores.sum() / num_train
+    
+    # Regularization
+    loss += reg * np.sum(W * W)
+    
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     #############################################################################
@@ -93,7 +132,8 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    # D x C
+    dW
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
